8bed855a7e165efe0b4a1daa2ae5e47069f72527
samborsol <ssboren.sce@gmail.com>
2025-10-23 20:02:53 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 import socket  # noqa: F401
+import threading
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
- 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept() #wait for client!
-    connection.sendall(b"+PONG\r\n")
+    connection, _ = server_socket.accept()
+    while True:
+        data = connection.recv(1024)
+        num = data.decode().count('PING')
+        if(num>0):
+           connection.sendall(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
